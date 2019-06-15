cc37d13c52ca51600fb3a777128c609f839b8d98
alexesmet <alexes.met@gmail.com>
2019-06-15 15:23:29 +0300

    grid for point-placement

--- map_editor.py
+++ map_editor.py
@@ -1,24 +1,37 @@
 import pygame
 # You can install pygame lib with `pip install pygame`
  
+class Bunch:
+    def __init__(self, **kwds):
+        self.__dict__.update(kwds)
+
 # Define some colors
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-GREEN = (0, 255, 0)
-RED = (255, 0, 0)
- 
+BLACK = (  0,  0,  0)
+WHITE = (255,255,255)
+
+points = []
+state = Bunch(
+    grid_size=20,
+    point_size=3
+)
+
+
+def snap_to_grid(pt):
+    if state.grid_size > 0:
+        gr = state.grid_size
+        return ((pt[0]+gr//2)//gr*gr,
+                (pt[1]+gr//2)//gr*gr)
+    else:
+        return pt
+
 pygame.init()
  
 # Set the width and height of the screen [width, height]
 size = (700, 500)
 screen = pygame.display.set_mode(size)
- 
 pygame.display.set_caption("My Game")
- 
-# Loop until the user clicks the close button.
+
 done = False
- 
-# Used to manage how fast the screen updates
 clock = pygame.time.Clock()
  
 # -------- Main Program Loop -----------
@@ -27,19 +40,29 @@ while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
- 
+        elif event.type == pygame.MOUSEBUTTONUP:
+            if event.button == 1:
+                points.append(snap_to_grid((event.pos[0],event.pos[1])))
     # --- Game logic should go here
  
     # --- Screen-clearing code goes here
-    screen.fill(WHITE)
+    screen.fill(BLACK)
  
     # --- Drawing code should go here
- 
-    # --- Go ahead and update the screen with what we've drawn.
-    pygame.display.flip()
- 
-    # --- Limit to 60 frames per second
-    clock.tick(60)
+    if state.grid_size > 0:
+        i = 0
+        while i < size[0]:
+            j = 0
+            while j < size[1]:
+                pygame.draw.circle(screen, (100,100,100), (i,j), state.point_size-2)
+                j += state.grid_size
+            i += state.grid_size
+
+    for point in points:
+        pygame.draw.circle(screen, WHITE, point, state.point_size)
+
+    pygame.display.flip() # swap buffers
+    clock.tick(60) # Limit to 60 frames per second
  
 # Close the window and quit.
 pygame.quit()
