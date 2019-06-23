c2aa934469315bd33042a47a0e0838e074bcf0ff
alexesmet <alexes.met@gmail.com>
2019-06-23 20:27:09 +0300

    refactoring level editor

--- map_editor.py
+++ map_editor.py
@@ -32,6 +32,72 @@ state = Bunch(
     dragging_point_id=None
 )
 
+def handle_event(event, state, level):
+    global done
+    global screen
+    global size
+    if event.type == pygame.QUIT:
+        done = True
+    if event.type == pygame.VIDEORESIZE:
+        old_surface_saved = screen
+        size = (event.w, event.h)
+        screen = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
+        screen.blit(old_surface_saved, (0,0))
+        del old_surface_saved
+    elif event.type == pygame.KEYDOWN:
+        if (event.key == pygame.K_LCTRL or event.key == pygame.K_RCTRL):
+            state.disable_grid_snapping = True
+        if (event.key == pygame.K_LALT):
+            state.moving_camera = True
+    elif event.type == pygame.KEYUP:
+        if (event.key == pygame.K_LCTRL or event.key == pygame.K_RCTRL):
+            state.disable_grid_snapping = False
+        if (event.key == pygame.K_LALT):
+            state.moving_camera = False
+
+    elif event.type == pygame.MOUSEBUTTONDOWN:
+        if event.button == 1:
+            if state.moving_camera:
+                state.moving_camera_from = event.pos
+                state.moving_camera_before = state.camera_shift
+            else:
+                for i, point in enumerate(level.points):
+                    pos = to_view(event.pos)
+                    if (pos[0]-point[0])**2+(pos[1]-point[1])**2<state.point_size**2+1:
+                        state.dragging_point_id = i
+                        break
+                else: # this is for-else expression, not if-else
+                    level.points.append(snap_to_grid(to_view(event.pos)))
+                    state.dragging_point_id = len(level.points)-1
+        elif event.button == 3:
+            for point in level.points:
+                pos = to_view(event.pos)
+                if (pos[0]-point[0])**2+(pos[1]-point[1])**2<state.point_size**2+1:
+                    level.points.remove(point)
+
+    elif event.type == pygame.MOUSEMOTION:
+        pos = to_view(event.pos)
+        if state.dragging_point_id is not None:
+            level.points[state.dragging_point_id] = snap_to_grid(pos)
+        if state.moving_camera_from is not None:
+            state.camera_shift = (
+                    state.moving_camera_before[0]-event.pos[0]+state.moving_camera_from[0],
+                    state.moving_camera_before[1]-event.pos[1]+state.moving_camera_from[1]
+                )
+
+    elif event.type == pygame.MOUSEBUTTONUP:
+        if event.button == 1:
+            state.dragging_point_id = None
+            state.moving_camera_from = None
+
+def to_view(pos, rev=False):
+    global state
+    if not rev:
+        return (pos[0]+state.camera_shift[0],
+                pos[1]+state.camera_shift[1])
+    else:
+        return (pos[0]-state.camera_shift[0],
+                pos[1]-state.camera_shift[1])
 
 def snap_to_grid(pt):
     if state.grid_size > 0 and not state.disable_grid_snapping:
@@ -55,56 +121,8 @@ clock = pygame.time.Clock()
 while not done:
     # --- Main event loop
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
-        if event.type == pygame.VIDEORESIZE:
-            old_surface_saved = screen
-            size = (event.w, event.h)
-            screen = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
-            screen.blit(old_surface_saved, (0,0))
-            del old_surface_saved
-        elif event.type == pygame.KEYDOWN:
-            if (event.key == pygame.K_LCTRL or event.key == pygame.K_RCTRL):
-                state.disable_grid_snapping = True
-            if (event.key == pygame.K_LALT):
-                state.moving_camera = True
-        elif event.type == pygame.KEYUP:
-            if (event.key == pygame.K_LCTRL or event.key == pygame.K_RCTRL):
-                state.disable_grid_snapping = False
-            if (event.key == pygame.K_LALT):
-                state.moving_camera = False
+        handle_event(event, state, level)
 
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:
-                if state.moving_camera:
-                    state.moving_camera_from = event.pos
-                    state.moving_camera_before = state.camera_shift
-                else:
-                    for i, point in enumerate(level.points):
-                        if (event.pos[0]+state.camera_shift[0]-point[0])**2+(event.pos[1]+state.camera_shift[1]-point[1])**2<state.point_size**2+1:
-                            state.dragging_point_id = i
-                            break
-                    else: # this is for-else expression, not if-else
-                        level.points.append(snap_to_grid((event.pos[0]+state.camera_shift[0],event.pos[1]+state.camera_shift[1])))
-                        state.dragging_point_id = len(level.points)-1
-            elif event.button == 3:
-                for point in level.points:
-                    if (event.pos[0]+state.camera_shift[0]-point[0])**2+(event.pos[1]+state.camera_shift[1]-point[1])**2<state.point_size**2+1:
-                        level.points.remove(point)
-
-        elif event.type == pygame.MOUSEMOTION:
-            if state.dragging_point_id is not None:
-                level.points[state.dragging_point_id] = snap_to_grid((event.pos[0]+state.camera_shift[0],event.pos[1]+state.camera_shift[1]))
-            if state.moving_camera_from is not None:
-                state.camera_shift = (
-                        state.moving_camera_before[0]-event.pos[0]+state.moving_camera_from[0],
-                        state.moving_camera_before[1]-event.pos[1]+state.moving_camera_from[1]
-                    )
-
-        elif event.type == pygame.MOUSEBUTTONUP:
-            if event.button == 1:
-                state.dragging_point_id = None
-                state.moving_camera_from = None
     # --- Game logic should go here
  
     # --- Screen-clearing code goes here
@@ -125,9 +143,7 @@ while not done:
             col = (255,255,0)
         else:
             col = WHITE
-        cam_point = (point[0]-state.camera_shift[0],
-                     point[1]-state.camera_shift[1])
-        pygame.draw.circle(screen, col, cam_point, state.point_size)
+        pygame.draw.circle(screen, col, to_view(point,True), state.point_size)
 
     pygame.display.flip() # swap buffers
     clock.tick(60) # Limit to 60 frames per second
